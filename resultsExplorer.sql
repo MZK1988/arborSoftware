678f7e086473d5d3a12e6b6eff4161ff4636e0bd
Max Korenvaes <max@korenvaes.com>
2019-11-11 17:38:38 -0600

    another commit

--- resultsExplorer.sql
+++ resultsExplorer.sql
@@ -1,36 +1,36 @@
-SELECT 
-copia.result.resultKey,
-copia.orderedpanel.orderedPanelKey,
-copia.CPT.cPTKey,
-copia.requisition.requisitionKey,
-copia.specimen.specimenKey,
-copia.test.testKey,
-copia.patient.patientKey,
-copia.orderedpanel.labFillerOrderNumber,
-copia.orderedpanel.labParentFillerOrderNumber,
-copia.Specimen.labOrderedStamp as [Lab Ordered Datetime],
-copia.Result.approvedStamp as [Result Approved Datetime],
-copia.Result.createStamp [Result Created Datetime],
-copia.Staff.firstName as [Provider First],
-copia.Staff.lastName as [Provider Last],
-copia.Staff.NPID,
-copia.Staff.professionalSuffix,
-copia.patient.firstName as [Patient First],
-copia.patient.lastName as [Patient Last],
-copia.patient.birthTimestamp as [Patient Birth Datetime],
-copia.organization.name as [Organization Name],
-copia.patient.isTestPatient,
-copia.orderedpanel.isCancelled as [orderIsCancelled],
-copia.OrderedPanel_CPT_Map.cptCount,
-copia.cpt.code as [CPT Code(s)],
-copia.panel.name as [Panel Name],
-copia.Result.labTestDescription,
-copia.test.description,
-copia.Result.techID [Approving Technician],
-copia.Result.numericPart,
-copia.Result.range,
-copia.result.result,
-copia.resultNote.note
+SELECT DISTINCT
+       copia.result.resultKey,
+       copia.orderedpanel.orderedPanelKey,
+       copia.CPT.cPTKey,
+       copia.requisition.requisitionKey,
+       copia.specimen.specimenKey,
+       copia.test.testKey,
+       copia.patient.patientKey,
+       copia.orderedpanel.labFillerOrderNumber,
+       copia.orderedpanel.labParentFillerOrderNumber,
+       copia.Specimen.labOrderedStamp as [Lab Ordered Datetime],
+       copia.Result.approvedStamp as [Result Approved Datetime],
+       copia.Result.createStamp [Result Created Datetime],
+       copia.Staff.firstName as [Provider First],
+       copia.Staff.lastName as [Provider Last],
+       copia.Staff.NPID,
+       copia.Staff.professionalSuffix,
+       copia.patient.firstName as [Patient First],
+       copia.patient.lastName as [Patient Last],
+       copia.patient.birthTimestamp as [Patient Birth Datetime],
+       copia.organization.name as [Organization Name],
+       copia.patient.isTestPatient,
+       copia.orderedpanel.isCancelled as [orderIsCancelled],
+       copia.OrderedPanel_CPT_Map.cptCount,
+       copia.cpt.code as [CPT Code(s)],
+       copia.panel.name as [Panel Name],
+       copia.Result.labTestDescription,
+       copia.test.description,
+       copia.Result.techID [Approving Technician],
+       copia.Result.numericPart,
+       copia.Result.range,
+       copia.result.result,
+       copia.resultNote.note
 FROM copia.Result
        LEFT JOIN copia.Test ON copia.result.testKey = copia.test.testKey
        LEFT JOIN copia.OrderedPanel ON copia.result.orderedPanelKey = copia.OrderedPanel.orderedPanelKey
@@ -41,8 +41,8 @@ FROM copia.Result
        LEFT JOIN copia.Specimen ON copia.orderedpanel.specimenKey = copia.specimen.specimenKey
        LEFT JOIN copia.Organization ON copia.requisition.organizationKey = copia.Organization.organizationKey
        LEFT JOIN copia.OrderedPanel_CPT_Map ON copia.orderedpanel.orderedPanelKey = copia.OrderedPanel_CPT_Map.orderedPanelKey
-       LEFT JOIN copia.CPT ON copia.OrderedPanel_CPT_Map.cptKey = copia.CPT.cPTKey 
-	   LEFT JOIN copia.staff ON copia.specimen.orderingPhysicianKey = copia.staff.staffKey
+       LEFT JOIN copia.CPT ON copia.OrderedPanel_CPT_Map.cptKey = copia.CPT.cPTKey
+       LEFT JOIN copia.staff ON copia.specimen.orderingPhysicianKey = copia.staff.staffKey
 --where copia.orderedPanel.isCancelled=1 means cancelled
 --only approximately 10,000/13mm records wherein the order is cancelled and a result is issued
 --need to do the attachment mapping
