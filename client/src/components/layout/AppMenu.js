9d190b4e257a13d466bb8929c68fb0ad85469726
MZK1988 <max@korenvaes.com>
2019-12-10 17:09:42 -0600

    another commit

--- client/src/components/layout/AppMenu.js
+++ client/src/components/layout/AppMenu.js
@@ -1,35 +1,36 @@
-import React, { Fragment, useState } from "react";
-import { Link, withRouter } from "react-router-dom";
+import React, { Fragment, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 const AppMenu = () => {
   const [displaySubMenus, toggleSubMenus] = useState(false);
 
   return (
     <Fragment>
-      <div class="sidebar">
-        <Link to="/dashboard">Home</Link>
-        <a href="#" onClick={() => toggleSubMenus(!displaySubMenus)}>
+      <div class='sidebar'>
+        <Link to='/dashboard'>Home</Link>
+        <a href='#' onClick={() => toggleSubMenus(!displaySubMenus)}>
           Operations
         </a>
         {displaySubMenus && (
           <Fragment>
-            <Link to="/terminal" id="operationsSub">
+            <Link to='/terminal' id='operationsSub'>
               > Terminal
             </Link>
 
-            <Link to="#news" id="operationsSub">
+            <Link to='#news' id='operationsSub'>
               > Warning Track
             </Link>
 
-            <Link to="#news" id="operationsSub">
+            <Link to='#news' id='operationsSub'>
               > Rejections
             </Link>
           </Fragment>
         )}
-        <a href="#contact">Customer Service</a>
-        <a href="#about">etc.</a>
+        <a href='#contact'>Customer Service</a>
+        <a href='#about'>etc.</a>
       </div>
     </Fragment>
   );
 };
+
 export default AppMenu;
